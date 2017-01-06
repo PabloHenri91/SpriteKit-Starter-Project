33c64d51f49a6a344a5d6e04fd9bba86bcf515d3
Pablo Henrique Bertaco <pablo_fonseca91@icloud.com>
2017-01-06 17:29:46 -0200

    + GameColorsExtension

---
+++ XCode/Shared/Game/GameColorsExtension.swift
@@ -0,0 +1,15 @@
+//
+//  GameColorsExtension.swift
+//  Hydra
+//
+//  Created by Pablo Henrique Bertaco on 1/6/17.
+//  Copyright © 2017 PabloHenri91. All rights reserved.
+//
+
+import SpriteKit
+
+extension GameColors {
+
+    static var someColor = #colorLiteral(red: 0, green: 0.5324168801, blue: 1, alpha: 1)
+
+}
